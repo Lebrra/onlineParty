da596f76216c3beec082213a3344fa094280c3f4
WesMcW <wesmcwhorter@gmail.com>
2021-03-27 13:26:40 -0500

    epic workz

---
+++ partyGame/Assets/Scripts/Game Board/BoardSpace.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardSpace : MonoBehaviour
+{
+    public int myNum, nextNum, prevNum;
+    public Transform pos1, pos2, pos3, pos4;
+}
