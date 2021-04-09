8d2adbb3821b3f335346bad04b249057b20fba4f
WesMcW <wesmcwhorter@gmail.com>
2021-04-09 00:07:43 -0500

    doodle jump

---
+++ partyGame/Assets/Scripts/DoodleJumpControles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoodleJumpControles : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float moveSpeed;
+    float movement = 0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal") * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 vel = rb.velocity;
+        vel.x = movement;
+        rb.velocity = vel;
+    }
+}
